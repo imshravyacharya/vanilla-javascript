39d8406725114689fd1b9b8a14fcab171d5b93da
Shravya Acharya <iamshravya.acharya@gmail.com>
2023-07-27 11:14:04 +0530

    feat
    alert and prompt

---
+++ app.js
@@ -0,0 +1,4 @@
+//console.log("Hello! The script is working from the different file");
+var usrname = prompt("Give me the username");
+var welcomeMessage = "This script works " + usrname;
+alert("Welcome! " + welcomeMessage);
