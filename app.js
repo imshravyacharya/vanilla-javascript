c4cf77ef0ef7ec9430897bdb78b45b9eb351dd68
Shravya Acharya <iamshravya.acharya@gmail.com>
2023-07-29 10:51:42 +0530

    feat:taking input and console

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-//console.log("Hello! The script is working from the different file");
-var usrname = prompt("Give me the username");
-var welcomeMessage = "This script works " + usrname;
-alert("Welcome! " + welcomeMessage);
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log(btnTranslate);
+
+function clickHandler() {
+  console.log("clicked!");
+  console.log(txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickHandler);
